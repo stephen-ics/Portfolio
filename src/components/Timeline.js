dc2d64841a501ffa54ff13efcb27654f26a19640
stephen-ics <stephenni1234@gmail.com>
2022-12-30 18:20:19 -0500

    timeline v2 rough

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,17 +1,44 @@
 import React from 'react'
-import TimelineData from '../data/TimelineData.js'
 import TimelineItem from './TimelineItem'
 import { Link } from 'react-router-dom'
+import WorkIcon from '../pictures/Logo.png';
+import SchoolIcon from '../pictures/Logo.png';
 
-import './component-styles/Timeline.css'
+import TimelineData from "../data/TimelineData.js";
 
-const Timeline = ({ id }) =>
-    TimelineData.length > 0 && (
-        <div className="timeline-container">
-            {TimelineData.map((data, idx) => (
-                <TimelineItem data={data} id={id} />
-            ))}
-        </div>
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
+const Timeline = () => {
+    let workIconStyles = { background: "#06D6A0" };
+    let schoolIconStyles = { background: "#f9c74f" };
+    return (
+        <div>
+        <h1 className="title">Timeline</h1>
+        <VerticalTimeline>
+          {TimelineData.map((element) => {
+            let isWorkIcon = element.icon === "work";
+            let showButton =
+              element.buttonText !== undefined &&
+              element.buttonText !== null &&
+              element.buttonText !== "";
+  
+            return (
+              <VerticalTimelineElement
+                key={element.key}
+                date={element.date}
+                dateClassName="date"
+                iconStyle={isWorkIcon ? workIconStyles : schoolIconStyles}
+                icon={isWorkIcon ? <WorkIcon /> : <SchoolIcon />}
+              >
+                  sadasd
+              </VerticalTimelineElement>
+            );
+          })}
+        </VerticalTimeline>
+      </div>
+
     );
+};
 
-export default Timeline;
+export default Timeline
